b853904a08ceb344dc89c8ca771de5885f533d44
nalifc <adamnuralifi11@gmail.com>
2023-09-18 10:34:09 +0700

    jobsheet 4

---
+++ hargaBayar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class hargaBayar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int harga, jumlah, jmlHalBuku;
+        String merkBuku;
+        double dis, total, bayar,jmlDis;
+
+        System.out.print("Merk buku yang dibeli : ");
+        merkBuku = input.nextLine();  
+        System.out.print("Jumlah halaman buku yang dibeli : ");
+        jmlHalBuku = input.nextInt(); 
+        System.out.print("Masukkan harga barang yang dibeli : ");
+        harga = input.nextInt();
+        System.out.print("Masukkan jumlah barang yang dibeli : ");
+        jumlah = input.nextInt();
+        System.out.print("Masukkan diskon yang didapatkan : ");
+        dis = input.nextDouble();
+        
+        total = jumlah*harga;
+        jmlDis = total*dis;
+        bayar = total-jmlDis;
+
+        System.out.println("Diskon yang ada dapat : "+jmlDis);
+        System.out.print("Jumlah yang harus dibayar : "+bayar);
+
+
+ 
+    }
+}
