1b34bc80c614b0557d9a512f8daa1b309fbafda3
nalifc <adamnuralifi11@gmail.com>
2023-09-26 09:01:01 +0700

    Jobsheet 5, Percobaan 2, Modifikasi

--- PemilihanPercobaan201.java
+++ PemilihanPercobaan201.java
@@ -16,8 +16,39 @@ public class PemilihanPercobaan201 {
         float tugas = input01.nextFloat();
 
         float total = (uas*0.4F) + (uts*0.3F) + (quiz*0.1F) + (tugas*0.2F);
-        String message = total < 65 ? "Remedi" : "Tidak Remedi";
-        System.out.printf("Nilai Akhir : %.2f Maka Anda %s ",total, message);
+        // String message = total < 65 ? "Remedi" : "Tidak Remedi";
+        // System.out.printf("Nilai Akhir : %.2f Maka Anda %s ",total, message);   
+        if (total > 80 && total <= 100) {
+            System.out.print("A ");
+            System.out.print("Nilai 4 ");
+            System.out.print("Sangat Baik");
+        } else if(total > 73 && total <= 80) {
+            System.out.print("B+ ");
+            System.out.print("Nilai 3.5 ");
+            System.out.print("Lebih Dari Baik");
+        }else if(total > 65 && total <= 73){
+            System.out.print("B ");
+            System.out.print("Nilai 3 ");
+            System.out.print("Baik");
+        }else if(total > 60 && total <= 65){
+            System.out.print("C+ ");
+            System.out.print("Nilai 2 ");
+            System.out.print("Lebih Dari Cukup"); 
+        }else if(total > 50 && total <= 60){
+            System.out.print("C ");
+            System.out.print("Nilai 2 ");
+            System.out.print("Cukup");
+        }else if (total > 39 && total <= 50) {
+            System.out.print("D ");
+            System.out.print("Nilai 1 ");
+            System.out.print("Kurang");
+        }else if (total <= 39) {
+            System.out.print("D ");
+            System.out.print("Nilai 0 ");
+            System.out.print("Gagal");
+        }else{
+            System.out.print("Terjadi Kesalahan");
+        }
     }
     
 }
